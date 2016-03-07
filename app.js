603eda07bbba904d6fc7bb1199502f6025a7c7ab
Rafael Walter <rafawalter@gmail.com>
2016-03-07 20:26:52 -0300

    hello ejs

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var http = require('http');
-var ip = 3000;
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+app.get('/produtos', function(req, res) {
+	res.render('produtos/lista');
+});
 
-var server = http.createServer(function(req,res) {
-	console.log('me chamou?');
-	res.writeHead(200, {ContentType: 'text/html'});
-	res.end("<html><body>Olá</body></html>");
-}).listen(ip, function() {
-	console.log('servidor iniciado');		
+app.listen(3000, function() {
+	console.log('servidor rodando');
 });
