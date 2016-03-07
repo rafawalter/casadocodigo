6a3a48fde55e42cf92f91314183aeddb4d821bab
Rafael Walter <rafawalter@gmail.com>
2016-03-07 19:16:48 -0300

    ola mundo

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var ip = 3000;
 
-var server = http.createServer()
-	.listen(ip, function() {
-		console.log('servidor iniciado');		
-	});
+var server = http.createServer(function(req,res) {
+	console.log('me chamou?');
+	res.writeHead(200, {ContentType: 'text/html'});
+	res.end("<html><body>Olá</body></html>");
+}).listen(ip, function() {
+	console.log('servidor iniciado');		
+});
