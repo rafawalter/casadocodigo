dbe415026bc46c2bed7e782aec8c310e01b02432
Rafael Walter <rafawalter@gmail.com>
2016-03-07 21:49:46 -0300

    listagem de livros

--- app/routes/produto.js
+++ app/routes/produto.js
@@ -1,5 +1,18 @@
+var mysql = require('mysql');
+
 module.exports = function(app) {
 	app.get('/produtos', function(req, res) {
-		res.render('produtos/lista');
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '',
+			database: 'casadocodigo',
+		});
+
+		connection.query('SELECT * FROM livros', function(erro, produtos) {
+			res.render('produtos/lista', {lista: produtos});
+		});
+
+		connection.end();
 	});
 };
--- config/express.js
+++ config/express.js
@@ -4,5 +4,7 @@ module.exports = function() {
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
 
+	app.use(express.static('./public'));
+
 	return app;
 };
