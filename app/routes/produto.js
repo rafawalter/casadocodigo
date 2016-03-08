0564ecf150de6a091b9649ac27ec977ede07d502
Rafael Walter <rafawalter@gmail.com>
2016-03-08 18:57:16 -0300

    connectionFactory

--- app/routes/produto.js
+++ app/routes/produto.js
@@ -1,13 +1,8 @@
-var mysql = require('mysql');
+var connectionFactory = require('../infra/connectionFactory');
 
 module.exports = function(app) {
 	app.get('/produtos', function(req, res) {
-		var connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: '',
-			database: 'casadocodigo',
-		});
+		var connection = connectionFactory();
 
 		connection.query('SELECT * FROM livros', function(erro, produtos) {
 			res.render('produtos/lista', {lista: produtos});
