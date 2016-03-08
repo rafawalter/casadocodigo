95aae6d5997bbd470ccf628bfa1ec56abd768cdb
Rafael Walter <rafawalter@gmail.com>
2016-03-08 19:56:16 -0300

    ProdutoDao

--- app/routes/produto.js
+++ app/routes/produto.js
@@ -1,10 +1,11 @@
 var connectionFactory = require('../infra/connectionFactory');
+var ProdutoDao = require('../infra/ProdutoDao');
 
 module.exports = function(app) {
 	app.get('/produtos', function(req, res) {
 		var connection = connectionFactory();
-
-		connection.query('SELECT * FROM livros', function(erro, produtos) {
+		var produtoDao = new ProdutoDao(connection);
+		produtoDao.lista( function(err, produtos) {
 			res.render('produtos/lista', {lista: produtos});
 		});
 
